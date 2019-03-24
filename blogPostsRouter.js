43ab323cbc7802835b0f51e098f3545fb38e9ad8
Sesha M <seshasirimachiraju@gmail.com>
2019-03-24 14:53:39 -0700

    added

---
+++ blogPostsRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('Harry Potter', 'A boy discovers he is a wizard', 'JK Rowling'); 
+BlogPosts.create('Emma','A wealthy girl plays matchmaker', 'Jane Austen'); 
+
+app.get('/', (req, res) => {
+ res.json(BlogPosts.get());
+});
+
+app.post('/', jsonParser, (req, res) => {
+ const requiredFields = ['title', 'content', 'author'];
+ for (let i=0; i<requiredFields.length; i++) {
+   const field = requiredFields[i];
+   if (!(field in req.body)) {
+     const message = `Missing \`${field}\` in request body`
+     console.error(message);
+     return res.status(400).send(message);
+   }
+ }
+ const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+ res.status(201).json(item);
+});
+
+app.put('/:id', jsonParser, (req, res) => {
+ const requiredFields = ['title', 'content', 'author', 'id'];
+ for (let i=0; i<requiredFields.length; i++) {
+   const field = requiredFields[i];
+   if (!(field in req.body)) {
+     const message = `Missing \`${field}\` in request body`
+     console.error(message);
+     return res.status(400).send(message);
+   }
+ }
+
+ if (req.params.id !== req.body.id) {
+   const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+   console.error(message);
+   return res.status(400).send(message);
+ }
+ console.log(`Updating blog post item \`${req.params.id}\``);
+ ShoppingList.update({
+   id: req.params.id,
+   title: req.body.title,
+   content: req.body.content, 
+   author: req.body.author,
+ });
+ res.status(204).end();
+});
+
+app.delete('/:id', (req, res) => {
+ BlogPosts.delete(req.params.id);
+ console.log(`Deleted shopping list item \`${req.params.ID}\``);
+ res.status(204).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 const express = require('express');
-const router = express.Router();
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+const blogPostRouter = require('./blogPostsRouter');
 
 // log the http layer
 app.use(morgan('common'));
 
-app.get('/blog-posts', (req, res) => {
- res.json(BlogPosts.get());
+app.use(express.static('public')); 
+
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+ console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
 
-app.post('/blog-posts', jsonParser, (req, res) => {
- // ensure `name` and `budget` are in request body
- const requiredFields = ['title', 'content', 'author'];
- for (let i=0; i<requiredFields.length; i++) {
-   const field = requiredFields[i];
-   if (!(field in req.body)) {
-     const message = `Missing \`${field}\` in request body`
-     console.error(message);
-     return res.status(400).send(message);
-   }
- }
 
- const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
- res.status(201).json(item);
-});
+
+
+
+
+
+
+
+
